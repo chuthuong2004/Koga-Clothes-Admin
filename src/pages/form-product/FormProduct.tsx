9f11a80bb1ed76cdddf6583163709aa97978a8f3
chuthuong2004 <chuthuong1080@gmail.com>
2023-12-11 00:04:50 +0700

    update form product

--- src/pages/form-product/FormProduct.tsx
+++ src/pages/form-product/FormProduct.tsx
@@ -7,6 +7,7 @@ type FormCreateProduct = {
   name: string;
   description: string;
   category: string;
+  gender: string[];
 }
 const FormProduct = () => {
   // const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
@@ -15,7 +16,8 @@ const FormProduct = () => {
     defaultValues: {
       name: '',
       description: '',
-      category: ''
+      category: '',
+      gender: []
     }
   })
   const onSubmit = (data: FormCreateProduct) => {
@@ -62,27 +64,27 @@ const FormProduct = () => {
 
                     )}
                   />
-                  {errors.name && <Typography.Text  type='danger'>{errors.name?.message}</Typography.Text>}
+                  {errors.name && <Typography.Text type='danger'>{errors.name?.message}</Typography.Text>}
                 </div>
                 <div className='flex flex-col'>
-                <Typography.Text>Mô tả</Typography.Text>
-                <Controller
-                  control={control}
-                  name="description"
-                  rules={{
-                    required: {
-                      value: true,
-                      message: 'Vui lòng nhập mô tả sản phẩm !'
-                    }
-                  }}
-                  render={({ field }) => (
-                    <Input size="large" placeholder="Enter product description" status={errors.description && 'error'} {...field} />
+                  <Typography.Text>Mô tả</Typography.Text>
+                  <Controller
+                    control={control}
+                    name="description"
+                    rules={{
+                      required: {
+                        value: true,
+                        message: 'Vui lòng nhập mô tả sản phẩm !'
+                      }
+                    }}
+                    render={({ field }) => (
+                      <Input size="large" placeholder="Enter product description" status={errors.description && 'error'} {...field} />
 
-                  )}
-                />
-                {errors.description && <Typography.Text type='danger'>{errors.description?.message}</Typography.Text>}
+                    )}
+                  />
+                  {errors.description && <Typography.Text type='danger'>{errors.description?.message}</Typography.Text>}
                 </div>
-                
+
                 {/* <Editor editorState={editorState} onChange={setEditorState} /> */}
               </Space>
             </Card>
@@ -95,34 +97,78 @@ const FormProduct = () => {
           <div className="flex flex-col gap-8">
             <Card bordered={false}>Pricing</Card>
             <Card bordered={false}>
-            <Typography.Text>Tổ chức</Typography.Text>
-                <Typography.Text>Danh mục</Typography.Text>
-                <Controller 
-                  control={control}
-                  name="category"
-                  rules={{
-                    required: {
-                      value: true,
-                      message: 'Vui lòng chọn danh mục !'
-                    }
-                  }}
-                  render={({field}) => (
-                    <Select
+              <Space direction='vertical' className='w-full'>
+                <Typography.Text>Tổ chức</Typography.Text>
+                <div className='flex flex-col gap-2'>
+                  <Typography.Text>Danh mục</Typography.Text>
+                  <Controller
+                    control={control}
+                    name="category"
+                    rules={{
+                      required: {
+                        value: true,
+                        message: 'Vui lòng chọn danh mục !'
+                      }
+                    }}
+                    render={({ field }) => (
+                      <Select
                         size='large'
                         defaultValue="10"
-                        style={{ width: 80 }}
                         {...field}
+                        status={errors.category && 'error'}
                         options={[
-                            { value: '5', label: '5' },
-                            { value: '10', label: '10' },
-                            { value: '20', label: '20' },
-                            { value: '25', label: '25' },
-                            { value: '50', label: '50' },
+                          { value: '', label: 'Chọn danh mục' },
+                          { value: '5', label: '5' },
+                          { value: '10', label: '10' },
+                          { value: '20', label: '20' },
+                          { value: '25', label: '25' },
+                          { value: '50', label: '50' },
                         ]}
-                    />
-                  )}
-                />
-                
+                      />
+                    )}
+                  />
+                  {
+                    errors.category &&
+                    <Typography.Text type='danger' >{errors.category?.message}</Typography.Text>
+                  }
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <Typography.Text>Collection</Typography.Text>
+                  <Controller
+                    control={control}
+                    name="category"
+                    rules={{
+                      required: {
+                        value: true,
+                        message: 'Vui lòng chọn collection !'
+                      }
+                    }}
+                    render={({ field }) => (
+                      <Select
+                        // mode="multiple"
+                        size='large'
+                        {...field}
+                        value={field.value && field.value}
+                        status={errors.category && 'error'}
+                        placeholder="Chọn collection"
+                        options={[
+                          // { value: '', label: 'Chọn collection' },
+                          { value: 'Man', label: "Man's Clothing" },
+                          { value: 'Woman', label: "Women's Clothing" },
+                          { value: 'Kids', label: "Kid's Clothing" },
+                        ]}
+                      />
+                    )}
+                  />
+                  {
+                    errors.category &&
+                    <Typography.Text type='danger' >{errors.category?.message}</Typography.Text>
+                  }
+                </div>
+
+
+              </Space>
+
             </Card>
           </div>
         </Col>
